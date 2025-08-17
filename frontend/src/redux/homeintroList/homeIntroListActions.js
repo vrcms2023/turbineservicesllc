31157de5cd39cda04d56b49301cb33ac2457d52b
Ramakrishna <ramajsworks@gmail.com>
2025-08-17 12:45:42 +0530

    update traing and corportate changes

--- frontend/src/redux/homeintroList/homeIntroListActions.js
+++ frontend/src/redux/homeintroList/homeIntroListActions.js
@@ -2,23 +2,18 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import { axiosClientServiceApi } from "../../util/axiosUtil";
 import { sortByFieldName } from "../../util/commonUtil";
 
-export const getHomeIntroList = createAsyncThunk(
-  "home/homeIntroList",
-  async (rejectWithValue) => {
-    try {
-      const { data } = await axiosClientServiceApi.get(
-        `carousel/clientHomeIntroList/HomeCorporate/`
-      );
+export const getHomeIntroList = createAsyncThunk("home/homeIntroList", async (rejectWithValue) => {
+  try {
+    const { data } = await axiosClientServiceApi.get(`carousel/clientHomeIntroList/homecorporate/`);
 
-      let response = sortByFieldName(data.intro, "intro_position");
-      return response;
-    } catch (error) {
-      // return custom error message from API if any
-      if (error.response && error.response.data.message) {
-        return rejectWithValue(error.response.data.message);
-      } else {
-        return rejectWithValue(error.message);
-      }
+    let response = sortByFieldName(data.intro, "intro_position");
+    return response;
+  } catch (error) {
+    // return custom error message from API if any
+    if (error.response && error.response.data.message) {
+      return rejectWithValue(error.response.data.message);
+    } else {
+      return rejectWithValue(error.message);
     }
   }
-);
+});
